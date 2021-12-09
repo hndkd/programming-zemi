23111d736b99de40b284393f6d04dc696193a933
hndkd <takoshomi0817@outlook.jp>
2021-12-09 09:51:22 +0000

    heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello-world' => "foo#hello"
-  resources :blogs
-  root 'blogs#index'
 end
