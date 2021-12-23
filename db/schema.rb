02d3016ff6c9aabe44cb55d75a966eb3ad51b391
hndkd <takoshomi0817@outlook.jp>
2021-12-23 11:30:33 +0000

    basic signup

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_121351) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "emil"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
