5e06812903e5c99158d2e02ab818a77840e16a1a
hndkd <takoshomi0817@outlook.jp>
2022-01-22 03:44:28 +0000

    follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_25_054232) do
+ActiveRecord::Schema.define(version: 2022_01_21_173903) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
